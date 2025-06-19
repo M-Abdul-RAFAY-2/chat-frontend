ed8dcb78d1e7035a502a4a2b07b8410f7a036b80
Abdul Rafay <130937186+M-Abdul-RAFAY@users.noreply.github.com>
2025-06-19 21:07:46 +0500

    sign-up and sign-in UI setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import { type Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,7 +36,6 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <Header />
           {children}
         </body>
       </html>
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+    <div className="flex pt-6 justify-center items-center min-h-screen bg-gray-100">
       <SignUp />
     </div>
   );
